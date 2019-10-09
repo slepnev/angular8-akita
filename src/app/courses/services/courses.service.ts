df85097e9292d9512e821072e46b0e0e9c922be3
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2019-10-09 08:33:56 +0300

    AKITA: Courses

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,62 +1,70 @@
-
-
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Course} from "../model/course";
-import {map} from "rxjs/operators";
-import {Lesson} from "../model/lesson";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { map } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
+import { CoursesStore } from '../state/courses.store';
 
 
 @Injectable()
 export class CoursesService {
 
-    constructor(private http:HttpClient) {
+  constructor(private http: HttpClient, private coursesStory: CoursesStore) {
+  }
 
-    }
+  findCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`/api/courses/${courseId}`);
+  }
 
-    findCourseById(courseId: number): Observable<Course> {
-        return this.http.get<Course>(`/api/courses/${courseId}`);
-    }
+  findAllCourses(): Observable<Course[]> {
+    return this.http.get('/api/courses')
+      .pipe(
+        map(res => res['payload'])
+      );
+  }
 
-    findAllCourses(): Observable<Course[]> {
-        return this.http.get('/api/courses')
-            .pipe(
-                map(res => res['payload'])
-            );
-    }
+  findAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get('/api/lessons', {
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('pageNumber', '0')
+        .set('pageSize', '1000')
+    }).pipe(
+      map(res => res['payload'])
+    );
+  }
 
-    findAllCourseLessons(courseId:number): Observable<Lesson[]> {
-        return this.http.get('/api/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('pageNumber', "0")
-                .set('pageSize', "1000")
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
+  findLessons(
+    courseId: number,
+    pageNumber = 0, pageSize = 3): Observable<Lesson[]> {
 
-    findLessons(
-        courseId:number,
-        pageNumber = 0, pageSize = 3):  Observable<Lesson[]> {
-
-        return this.http.get('/api/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('filter', '')
-                .set('sortOrder', 'asc')
-                .set('pageNumber', pageNumber.toString())
-                .set('pageSize', pageSize.toString())
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
+    return this.http.get('/api/lessons', {
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('filter', '')
+        .set('sortOrder', 'asc')
+        .set('pageNumber', pageNumber.toString())
+        .set('pageSize', pageSize.toString())
+    }).pipe(
+      map(res => res['payload'])
+    );
+  }
 
 
-    saveCourse(courseId: number, changes: Partial<Course>) {
-        return this.http.put('/api/courses/' + courseId, changes);
-    }
+  saveCourse(courseId: number, changes: Partial<Course>) {
+    return this.http.put('/api/courses/' + courseId, changes);
+  }
 
+  courseLoaded(course) {
+    this.coursesStory.add(course);
+  }
+
+  allCoursesLoaded(courses) {
+    this.coursesStory.add(courses);
+    this.coursesStory.update({
+      allCoursesLoaded: true
+    });
+  }
 
 }
--- src/app/courses/state/courses.query.ts
+++ src/app/courses/state/courses.query.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Query } from '@datorama/akita';
-import { CoursesStore, CoursesState } from './courses.store';
+import { QueryEntity } from '@datorama/akita';
+import { CoursesState, CoursesStore } from './courses.store';
 
-@Injectable({ providedIn: 'root' })
-export class CoursesQuery extends Query<CoursesState> {
+@Injectable({providedIn: 'root'})
+export class CoursesQuery extends QueryEntity<CoursesState> {
+
+  beginnerCourses$ = this.selectAll({
+    filterBy: course => course.category === 'BEGINNER'
+  });
+  advancedCourses$ = this.selectAll({
+    filterBy: course => course.category === 'ADVANCED'
+  });
+  promoTotal$ = this.selectCount(course => course.promo);
 
   constructor(protected store: CoursesStore) {
     super(store);
+  }
+
+  courseById(id: number) {
+    return this.getEntity(id);
   }
 
 }
